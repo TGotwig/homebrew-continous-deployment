0a740ef59f108d4285cb28fa80b459d6d42d47fc
runner <runner@Mac-1651954739357.local>
2022-05-07 20:29:12 +0000

    continous-deployment-test 0.0.1

--- continous-deployment-test.rb
+++ continous-deployment-test.rb
@@ -1,8 +1,9 @@
 class ContinousDeploymentTest < Formula
   desc "desc... :-)"
   homepage "https://github.com/TGotwig/continous-deployment-test"
-  url "https://github.com/tgotwig/continous-deployment-test/releases/download/0.0.0/continous-deployment-test-mac.tar.gz"
-  sha256 "6d0f2daa78bafbbebb838320b568557676abd89ac013e6395f460b00f65cd9fb"
+  url "https://github.com/tgotwig/continous-deployment-test/releases/download/0.0.1/continous-deployment-test-mac.tar.gz"
+  version "0.0.1"
+  sha256 "cea933ff3fa6fe8c8eade17bd45a4c9371a7206082296d2094816b8419274e9a"
   
   def install
     bin.install "continous-deployment-test"
