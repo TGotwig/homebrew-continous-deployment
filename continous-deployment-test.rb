d163bab5929499105c460ec1d492fac606e202ba
Restyled.io <commits@restyled.io>
2022-05-07 20:29:18 +0000

    Restyled by standardrb

--- continous-deployment-test.rb
+++ continous-deployment-test.rb
@@ -4,7 +4,7 @@ class ContinousDeploymentTest < Formula
   url "https://github.com/tgotwig/continous-deployment-test/releases/download/0.0.1/continous-deployment-test-mac.tar.gz"
   version "0.0.1"
   sha256 "cea933ff3fa6fe8c8eade17bd45a4c9371a7206082296d2094816b8419274e9a"
-  
+
   def install
     bin.install "continous-deployment-test"
   end
