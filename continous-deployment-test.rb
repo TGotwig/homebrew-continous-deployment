116a7889ef460f339cf84ce50b26e0d7c8324ba2
Thomas Gotwig <github@tomasu.mozmail.com>
2022-05-07 21:17:51 +0200

    Update continous-deployment-test.rb

--- continous-deployment-test.rb
+++ continous-deployment-test.rb
@@ -1,1 +1,12 @@
+class ContinousDeploymentTest < Formula
+  desc "desc... :-)"
+  homepage "https://github.com/TGotwig/continous-deployment-test"
+  
+  def install
+    bin.install "continous-deployment-test"
+  end
 
+  test do
+    assert true
+  end
+end
