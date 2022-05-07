42a77cf944e449c80c582a62ef1e39e19d765eab
Thomas Gotwig <github@tomasu.mozmail.com>
2022-05-07 21:54:38 +0200

    Update continous-deployment-test.rb

--- continous-deployment-test.rb
+++ continous-deployment-test.rb
@@ -2,7 +2,7 @@ class ContinousDeploymentTest < Formula
   desc "desc... :-)"
   homepage "https://github.com/TGotwig/continous-deployment-test"
   url "https://github.com/tgotwig/continous-deployment-test/releases/download/0.0.0/continous-deployment-test-mac.tar.gz"
-  sha256 ""
+  sha256 "6d0f2daa78bafbbebb838320b568557676abd89ac013e6395f460b00f65cd9fb"
   
   def install
     bin.install "continous-deployment-test"
