cc25e62eb97bed19a7e2e6d5a60413f91e301e9b
Thomas Gotwig <github@tomasu.mozmail.com>
2022-05-08 08:39:34 +0200

    Fix typo in project name

--- continous-deployment.rb
+++ continous-deployment.rb
@@ -1,12 +1,12 @@
-class ContinousDeployment < Formula
+class ContinuousDeployment < Formula
   desc "desc... :-)"
-  homepage "https://github.com/TGotwig/continous-deployment"
-  url "https://github.com/tgotwig/continous-deployment/releases/download/0.0.1/continous-deployment-mac.tar.gz"
+  homepage "https://github.com/TGotwig/continuous-deployment"
+  url "https://github.com/tgotwig/continuous-deployment/releases/download/0.0.1/continuous-deployment-mac.tar.gz"
   version "0.0.1"
   sha256 "39f2b62d074f85efa3e432651564497688383a588ec90ef4ed7b3f37c2fab186"
 
   def install
-    bin.install "continous-deployment"
+    bin.install "continuous-deployment"
   end
 
   test do
