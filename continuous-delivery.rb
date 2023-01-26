05e1124fead2b377686f44d680552336ed1ef986
runner <runner@Mac-1674739643627.local>
2023-01-26 13:31:42 +0000

    continuous-delivery 0.0.6

--- continuous-delivery.rb
+++ continuous-delivery.rb
@@ -1,9 +1,9 @@
 class ContinuousDelivery < Formula
   desc "desc... :-)"
   homepage "https://github.com/TGotwig/continuous-delivery"
-  url "https://github.com/tgotwig/continuous-delivery/releases/download/0.0.3/continuous-delivery-mac.tar.gz"
-  version "0.0.3"
-  sha256 "c4d5d3dfbf0ad8ce037f278ff07d9534432536cdd8d2c20fef7fb8ce4f3b92ec"
+  url "https://github.com/tgotwig/continuous-delivery/releases/download/0.0.6/continuous-delivery-mac.tar.gz"
+  version "0.0.6"
+  sha256 "63d593640cc0b4d9bca553d37c7dffeb04125ddcf7281f12ac2cc45cdb43910d"
 
   def install
     bin.install "continuous-delivery"
